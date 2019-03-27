4123feec9de694b1b678a0a77dbc28a7d31aeadf
joe <42183110+xiaomaofeng@users.noreply.github.com>
2019-03-27 22:53:04 +0800

    update

---
+++ algorithm/cpp/QuickSort.cpp
@@ -0,0 +1,36 @@
+#include<algorithm>
+using namespace std;
+template <typename ElementType>
+void split(ElementType x[], int first, int last)
+{
+	ElementType pivot = x[first];
+	int left = first;
+	int right = last;
+	while (left < right)
+	{
+		while (pivot < x[right])
+			right--;
+		while (left < right && (x[left] < pivot || x[left] == pivot))
+			left++;
+		if (left < right)
+			swap(x[left], x[right]);
+
+			
+	}
+	int pos = right;
+	x[first] = x[pos];
+	x[pos] = pivot;
+	return pos;
+}
+
+template <typename ElementType>
+void quicksort(ElementType x[], int first, int last)
+{
+	int pos;
+	if (first < last)
+	{
+		pos = split(x, first, last, pos);
+		quicksort(x, first, pos - 1);
+		quicksort(x, pos + 1, last); 
+	}
+}
